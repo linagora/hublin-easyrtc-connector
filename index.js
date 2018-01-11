bfcab42a4b85257b4f955aeacb0ea7cfa805450a
Christophe Hamerling <chamerling@linagora.com>
2018-01-11 15:54:05 +0100

    linagora/lgs/openpaas/linagora.esn.webrtc#526 Use webrtc registry

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 const AwesomeModule = require('awesome-module');
 const Dependency = AwesomeModule.AwesomeModuleDependency;
+const MODULE_NAME = 'hublin.easyrtc.connector';
 
-const easyRTCConnector = new AwesomeModule('hublin.easyrtc.connector', {
+const easyRTCConnector = new AwesomeModule(MODULE_NAME, {
   dependencies: [
     new Dependency(Dependency.TYPE_ABILITY, 'wsserver', 'wsserver'),
     new Dependency(Dependency.TYPE_NAME, 'webserver.wrapper', 'webserver-wrapper'),
-    new Dependency(Dependency.TYPE_NAME, 'linagora.io.meetings.core.logger', 'logger')
+    new Dependency(Dependency.TYPE_NAME, 'linagora.io.meetings.core.logger', 'logger'),
+    new Dependency(Dependency.TYPE_NAME, 'linagora.esn.webrtc', 'webrtc')
   ],
-  abilities: ['hublin.webrtc.connector'],
+  abilities: ['webrtc-adapter'],
   states: {
     lib: (dependencies, callback) => {
-      callback(null, {
-        lib: require('./backend/lib')(dependencies)
-      });
+      const webrtc = dependencies('webrtc');
+      const lib = require('./backend/lib')(dependencies);
+
+      webrtc.registry.register(MODULE_NAME, lib);
+
+      callback(null, { lib });
     },
 
     deploy: (dependencies, callback) => {
       const app = require('./backend/webserver/application')();
       const webserverWrapper = dependencies('webserver-wrapper');
 
-      webserverWrapper.injectAngularModules('connector', ['app.js', 'services/easyRTCAdapter.js'], 'hublin.easyrtc.connector', ['live-conference']);
+      webserverWrapper.injectAngularModules('connector', ['app.js', 'services/easyRTCAdapter.js'], MODULE_NAME, ['live-conference']);
       webserverWrapper.injectJSAsset('connector', ['easyrtc/easyrtc.js', 'easyrtc/labs/easyrtc_rates.js'], ['connector']);
       webserverWrapper.addApp('connector', app);
 
